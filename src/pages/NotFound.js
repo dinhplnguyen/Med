49c2933f704b9608a6210ef24b66aed5c7f6af2d
Dinh Phi Long Nguyen <superitboss@gmail.com>
2022-03-23 22:55:32 -0700

    Update NotFound.js

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
-  return (
-    <React.Fragment>
-      <h1>404: Page Not Found</h1>
-    </React.Fragment>
-  );
+  <h1>404: Page Not Found</h1>;
 };
 export default NotFoundPage;
